1981f595abf577fb87d426e898bb927589b116b7
MARTA GARCIA <martagj94@gmail.com>
2023-04-19 18:02:23 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,67 @@
+import React from 'react';
+import './App.css';
+import { QueryClient, QueryClientProvider, useQuery } from "react-query";
+import { addScheduleTimes } from './utilities/times.js';
+import CourseList from './components/CourseList';
+
+
+const Banner = ({ title }) => (
+  <h1>{ title }</h1>
+);
+
+const fetchSchedule = async () => {
+  const url = 'https://courses.cs.northwestern.edu/394/data/cs-courses.php';
+  const response = await fetch(url);
+  if (!response.ok) throw response;
+  return addScheduleTimes(await response.json());
+};
+
+// const schedule = {
+//   "title": "CS Courses for 2018-2019",
+//   "courses": {
+//     "F101" : {
+//       "id" : "F101",
+//       "meets" : "MWF 11:00-11:50",
+//       "title" : "Computer Science: Concepts, Philosophy, and Connections"
+//     },
+//     "F110" : {
+//       "id" : "F110",
+//       "meets" : "MWF 10:00-10:50",
+//       "title" : "Intro Programming for non-majors"
+//     },
+//     "S313" : {
+//       "id" : "S313",
+//       "meets" : "TuTh 15:30-16:50",
+//       "title" : "Tangible Interaction Design and Learning"
+//     },
+//     "S314" : {
+//       "id" : "S314",
+//       "meets" : "TuTh 9:30-10:50",
+//       "title" : "Tech & Human Interaction"
+//     }
+//   }
+// };
+
+const Main = () =>  {
+  const { data: schedule, isLoading, error } = useQuery('schedule', fetchSchedule);
+  
+  if (error) return <h1>{error}</h1>;
+  if (isLoading) return <h1>Loading the schedule...</h1>
+
+  return (
+    <div className="container">
+      <Banner title={ schedule.title } />
+      <CourseList courses={ schedule.courses } /> 
+    </div>
+  );
+};
+
+const queryClient = new QueryClient();
+
+const App = () => (
+  <QueryClientProvider client={queryClient}>
+    <Main />
+  </QueryClientProvider>
+); 
+
+export default App;
